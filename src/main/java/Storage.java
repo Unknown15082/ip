c751577bdb0a316963abeda7bdbfea8f3c48d228
Unknown15082 <trangiahuy15082006@gmail.com>
2026-01-23 01:39:03 +0800

    feat: make Storage general

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,21 +1,21 @@
 import java.util.ArrayList;
 
-public class Storage {
-    private final ArrayList<String> memory;
+public class Storage<T> {
+    private final ArrayList<T> memory;
 
     public Storage() {
         memory = new ArrayList<>(120);
     }
 
-    public void add(String item) {
+    public void add(T item) {
         memory.add(item);
     }
 
     public String getAllAsString() {
         StringBuilder str = new StringBuilder();
         for (int i = 0; i < memory.size(); i++) {
-            String item = memory.get(i);
-            str.append(String.format("%d: %s", i+1, item));
+            T item = memory.get(i);
+            str.append(String.format("%d: %s", i+1, item.toString()));
             if (i < memory.size() - 1) str.append("\n");
         }
         return str.toString();
