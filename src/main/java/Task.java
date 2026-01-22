cd5201c3dbe42043fe52d03a620ed17708a9f7ea
Unknown15082 <trangiahuy15082006@gmail.com>
2026-01-23 01:36:38 +0800

    feat: add class Task to store any task

---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    private final String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDisplay() {
+        return String.format("[%s] %s", this.getStatusIcon(), this.description);
+    }
+
+    private String getStatusIcon() {
+        return this.isDone ? "X" : " ";
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+}
