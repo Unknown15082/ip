cddca9cbffee3776d8217c15cee4f562f8066a20
Unknown15082 <trangiahuy15082006@gmail.com>
2026-02-03 23:27:51 +0800

    Add tests

---
+++ src/test/java/airis/StorageTest.java
@@ -0,0 +1,41 @@
+package airis;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StorageTest {
+    @Test
+    public void addTest() {
+        Storage storage = new Storage();
+        Task task = new Task("test");
+        storage.add(task);
+        assertEquals(task, storage.get(0));
+    }
+
+    @Test
+    public void addTest_multiple() {
+        Storage storage = new Storage();
+        Task task1 = new Task("test");
+        Task task2 = new Task("try");
+        Task task3 = new Task("attempt");
+        storage.add(task1);
+        storage.add(task2);
+        storage.add(task3);
+        assertEquals(task3, storage.get(2));
+    }
+
+    @Test
+    public void removeTest() {
+        Storage storage = new Storage();
+        Task task1 = new Task("test");
+        Task task2 = new Task("try");
+        Task task3 = new Task("attempt");
+        storage.add(task1);
+        storage.add(task2);
+        storage.add(task3);
+        storage.remove(2);
+        assertEquals(task2, storage.get(1));
+        storage.remove(0);
+        assertEquals(task2, storage.get(0));
+    }
+}
---
+++ src/test/java/airis/TaskTest.java
@@ -0,0 +1,48 @@
+package airis;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskTest {
+    @Test
+    public void loadTaskTest_Todo() throws AirisException {
+        Task task = Task.loadTask("T|false|sleep");
+        assertEquals("[T][ ] sleep", task.toString());
+    }
+
+    @Test
+    public void loadTaskTest_Deadline() throws AirisException {
+        Task task = Task.loadTask("D|false|iP|Feb 06 2026 16:00");
+        assertEquals("[D][ ] iP (by: 2026-02-06 16:00:00)", task.toString());
+    }
+
+    @Test
+    public void loadTaskTest_Event() throws AirisException {
+        Task task = Task.loadTask(
+                "E|false|meeting|Feb 04 2026 18:00|Feb 04 2026 20:00");
+        assertEquals(
+                "[E][ ] meeting (from: 2026-02-04 18:00:00; by: 2026-02-04 20:00:00)",
+                task.toString()
+        );
+    }
+
+    @Test
+    public void loadTaskTest_done() throws AirisException {
+        Task task = Task.loadTask("T|true|homework");
+        assertEquals("[T][X] homework", task.toString());
+    }
+
+    @Test
+    public void markAsDoneTest() {
+        Task task = new Task("test", false);
+        task.markAsDone();
+        assertEquals("[X] test", task.toString());
+    }
+
+    @Test
+    public void markAsNotDoneTest() {
+        Task task = new Task("documentation", true);
+        task.markAsNotDone();
+        assertEquals("[ ] documentation", task.toString());
+    }
+}
